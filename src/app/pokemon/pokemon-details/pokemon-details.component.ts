e13c212bb80df1e523c638c7fbb55f12bd6a7d05
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-12-27 10:48:10 +0100

    [FEATURE] Add function for choosen or not in pokemon-details

--- src/app/pokemon/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Pokemon} from "../pokemon";
+import {PokemonService} from "../pokemon.service";
 
 @Component({
   selector: 'app-pokemon-details',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-details.component.sass']
 })
 export class PokemonDetailsComponent implements OnInit {
+  public pokemon: Pokemon = new Pokemon();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private service: PokemonService) {
+    let id = +this.route.snapshot.params['id'];
+    this.service.getPokemonById(id)
+      .subscribe(
+        response => {
+          this.pokemon = new Pokemon(response.name, "https://pokeapi.com/api/v1/pokemon/"+id+"/");
+          this.pokemon.setDetails(response)
+          console.log(this.pokemon)
+        },
+        error => {
+          console.log(error)
+        }
+      );
+  }
 
   ngOnInit() {
+
+
   }
 
 }
