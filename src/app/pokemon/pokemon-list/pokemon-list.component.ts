7c7199950455b38143fb108be20398f4ff9542ee
younesboulkaddid <boulkaddid.younes@gmail.com>
2016-12-27 07:56:15 +0100

    [FEATURE] Add Pokemon List component base function

--- src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PokemonService} from "../pokemon.service";
+import {Pokemon} from "../pokemon";
 
 @Component({
   selector: 'app-pokemon-list',
@@ -6,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-list.component.sass']
 })
 export class PokemonListComponent implements OnInit {
+  private pokemons: Pokemon[] = [];
 
-  constructor() { }
+  constructor(private service: PokemonService) {
+    this.getPokemons(0);
+  }
+
+  getPokemons(page){
+    this.service.getPokemons(page)
+      .subscribe(
+        response => {
+          if(response.next){
+            var superThis = this;
+            setTimeout(function(){
+              superThis.getPokemons(++page);
+            }, 2000)
+          }
+          for(var i in response.results){
+            var name = response.results[i].name;
+            var url = response.results[i].url;
+            this.pokemons.push(new Pokemon(name, url))
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      );
+  }
 
   ngOnInit() {
   }
